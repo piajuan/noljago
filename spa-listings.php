5657cfae9dedd6eb90d9892ac9962faf9aaf5f3f
piajuan <piajuan.dev@gmail.com>
2022-04-08 02:59:06 +0800

    refactor: change styleguide

--- spa-listings.php
+++ spa-listings.php
@@ -11,30 +11,27 @@ include("header.php");
                     <div class="nj-hero__search">
                         <div class="nj-hero__search-item">
                             <div class="frm__grp">
+                                <input class="frm__input frm__input--bold" type="text" placeholder="what do you want to do?">
                                 <div class="frm__append">
                                     <i class="frm__icon fa-solid fa-magnifying-glass-location"></i>
                                 </div>
-                                <input class="frm__input frm__input--bold" type="text" placeholder="what do you want to do?">
                             </div>
                         </div>
                         <div class="nj-hero__search-item">
                             <div class="frm__grp">
-                                <!-- <div class="frm__append">
-                                    <i class="frm__icon fa-regular fa-magnifying-glass-location"></i>
-                                </div> -->
                                 <input class="frm__input frm__input--bold" type="date" placeholder="when you want to go?">
                             </div>
                         </div>
                         <div class="nj-hero__search-item">
                             <div class="frm__grp">
+                                <select class="frm__input frm__input--bold" name="" id="">
+                                    <option value="">Option 1</option>
+                                    <option value="">Option 2</option>
+                                    <option value="">Option 3</option>
+                                </select>
                                 <div class="frm__append">
                                     <i class="frm__icon fa-solid fa-circle-chevron-down"></i>
                                 </div>
-                                <select class="frm__input frm__input--bold" name="" id="">
-                                    <option value="">Solo</option>
-                                    <option value="">Lorem</option>
-                                    <option value="">Lorem</option>
-                                </select>
                             </div>
                         </div>
                     </div>
@@ -88,6 +85,7 @@ include("header.php");
                                 <div class="nj-action-widget">
                                     <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-heart"></i></button>
                                     <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-share-nodes"></i></button>
+                                    <button class="btn btn--circle btn--orange"><i class="fa-solid fa-plus"></i></button>
                                 </div>
                                 <img src="assets/img/badge/top-rated.png" class="nj-badge" />
                                 <span class="nj-location"><i class="fa-solid fa-location-dot me-1"></i>Station 2, Boracay</span>
@@ -105,13 +103,6 @@ include("header.php");
                                 </p>
                             </div>
 
-                            <div class="nj-engagement d-flex justify-content-between">
-                                <div>
-                                    <span class="nj-engagement__reacts me-2"><i class="fa-regular fa-face-smile me-1"></i>3262</span>
-                                    <span class="nj-engagement__reacts"><i class="fa-regular fa-face-frown me-1"></i>76</span>
-                                </div>
-                                <span class="nj-engagement__replies"><i class="fa-solid fa-reply me-1"></i>234 Replies</span>
-                            </div>
                             <div class="text-center mt-2">
                                 <a href="#" class="btn btn--blue">Book Now</a>
 
@@ -121,215 +112,7 @@ include("header.php");
                     </div>
                     <!--/Listing Item -->
 
-                    <!-- Listing Item -->
-                    <div class="nj-listings__item">
-                        <div class="nj-listings__card">
-                            <img class="nj-thumb" src="assets/img/spa/l-2.jpeg" alt="">
-                            <div class="nj-content">
-                                <span class="tag tag--lime">Save as much as 15%</span>
-                                <div class="nj-action-widget">
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-heart"></i></button>
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-share-nodes"></i></button>
-                                </div>
-                                <img src="assets/img/badge/top-rated.png" class="nj-badge" />
-                                <span class="nj-location"><i class="fa-solid fa-location-dot me-1"></i>Station 2, Boracay</span>
-                            </div>
-                        </div>
-                        <div class="p-3">
-                            <h3 class="nj-title">Bella Isa Salon & Spa</h3>
-                            <div class="nj-price-wrapper">
-                                <span class="nj-label">Starts</span>
-                                <span class="nj-price">P1,000</span>
-                            </div>
-                            <div class="nj-excerpt line-clamp">
-                                <p class="">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates
-                                </p>
-                            </div>
 
-                            <div class="nj-engagement d-flex justify-content-between">
-                                <div>
-                                    <span class="nj-engagement__reacts me-2"><i class="fa-regular fa-face-smile me-1"></i>3262</span>
-                                    <span class="nj-engagement__reacts"><i class="fa-regular fa-face-frown me-1"></i>76</span>
-                                </div>
-                                <span class="nj-engagement__replies"><i class="fa-solid fa-reply me-1"></i>234 Replies</span>
-                            </div>
-                            <div class="text-center mt-2">
-                                <a href="#" class="btn btn--blue">Book Now</a>
-
-                            </div>
-                        </div>
-
-                    </div>
-                    <!--/Listing Item -->
-
-                    <!-- Listing Item -->
-                    <div class="nj-listings__item">
-                        <div class="nj-listings__card">
-                            <img class="nj-thumb" src="assets/img/spa/l-5.jpeg" alt="">
-                            <div class="nj-content">
-                                <span class="tag tag--lime">Save as much as 15%</span>
-                                <div class="nj-action-widget">
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-heart"></i></button>
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-share-nodes"></i></button>
-                                </div>
-                                <img src="assets/img/badge/top-rated.png" class="nj-badge" />
-                                <span class="nj-location"><i class="fa-solid fa-location-dot me-1"></i>Station 2, Boracay</span>
-                            </div>
-                        </div>
-                        <div class="p-3">
-                            <h3 class="nj-title">Bella Isa Salon & Spa</h3>
-                            <div class="nj-price-wrapper">
-                                <span class="nj-label">Starts</span>
-                                <span class="nj-price">P1,000</span>
-                            </div>
-                            <div class="nj-excerpt line-clamp">
-                                <p class="">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates
-                                </p>
-                            </div>
-
-                            <div class="nj-engagement d-flex justify-content-between">
-                                <div>
-                                    <span class="nj-engagement__reacts me-2"><i class="fa-regular fa-face-smile me-1"></i>3262</span>
-                                    <span class="nj-engagement__reacts"><i class="fa-regular fa-face-frown me-1"></i>76</span>
-                                </div>
-                                <span class="nj-engagement__replies"><i class="fa-solid fa-reply me-1"></i>234 Replies</span>
-                            </div>
-                            <div class="text-center mt-2">
-                                <a href="#" class="btn btn--blue">Book Now</a>
-
-                            </div>
-                        </div>
-
-                    </div>
-                    <!--/Listing Item -->
-
-                    <!-- Listing Item -->
-                    <div class="nj-listings__item">
-                        <div class="nj-listings__card">
-                            <img class="nj-thumb" src="assets/img/spa/l-3.jpeg" alt="">
-                            <div class="nj-content">
-                                <span class="tag tag--lime">Save as much as 15%</span>
-                                <div class="nj-action-widget">
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-heart"></i></button>
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-share-nodes"></i></button>
-                                </div>
-                                <img src="assets/img/badge/top-rated.png" class="nj-badge" />
-                                <span class="nj-location"><i class="fa-solid fa-location-dot me-1"></i>Station 2, Boracay</span>
-                            </div>
-                        </div>
-                        <div class="p-3">
-                            <h3 class="nj-title">Bella Isa Salon & Spa</h3>
-                            <div class="nj-price-wrapper">
-                                <span class="nj-label">Starts</span>
-                                <span class="nj-price">P1,000</span>
-                            </div>
-                            <div class="nj-excerpt line-clamp">
-                                <p class="">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates
-                                </p>
-                            </div>
-
-                            <div class="nj-engagement d-flex justify-content-between">
-                                <div>
-                                    <span class="nj-engagement__reacts me-2"><i class="fa-regular fa-face-smile me-1"></i>3262</span>
-                                    <span class="nj-engagement__reacts"><i class="fa-regular fa-face-frown me-1"></i>76</span>
-                                </div>
-                                <span class="nj-engagement__replies"><i class="fa-solid fa-reply me-1"></i>234 Replies</span>
-                            </div>
-                            <div class="text-center mt-2">
-                                <a href="#" class="btn btn--blue">Book Now</a>
-
-                            </div>
-                        </div>
-
-                    </div>
-                    <!--/Listing Item -->
-
-                    <!-- Listing Item -->
-                    <div class="nj-listings__item">
-                        <div class="nj-listings__card">
-                            <img class="nj-thumb" src="assets/img/spa/l-6.jpeg" alt="">
-                            <div class="nj-content">
-                                <span class="tag tag--lime">Save as much as 15%</span>
-                                <div class="nj-action-widget">
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-heart"></i></button>
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-share-nodes"></i></button>
-                                </div>
-                                <img src="assets/img/badge/top-rated.png" class="nj-badge" />
-                                <span class="nj-location"><i class="fa-solid fa-location-dot me-1"></i>Station 2, Boracay</span>
-                            </div>
-                        </div>
-                        <div class="p-3">
-                            <h3 class="nj-title">Bella Isa Salon & Spa</h3>
-                            <div class="nj-price-wrapper">
-                                <span class="nj-label">Starts</span>
-                                <span class="nj-price">P1,000</span>
-                            </div>
-                            <div class="nj-excerpt line-clamp">
-                                <p class="">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates
-                                </p>
-                            </div>
-
-                            <div class="nj-engagement d-flex justify-content-between">
-                                <div>
-                                    <span class="nj-engagement__reacts me-2"><i class="fa-regular fa-face-smile me-1"></i>3262</span>
-                                    <span class="nj-engagement__reacts"><i class="fa-regular fa-face-frown me-1"></i>76</span>
-                                </div>
-                                <span class="nj-engagement__replies"><i class="fa-solid fa-reply me-1"></i>234 Replies</span>
-                            </div>
-                            <div class="text-center mt-2">
-                                <a href="#" class="btn btn--blue">Book Now</a>
-
-                            </div>
-                        </div>
-
-                    </div>
-                    <!--/Listing Item -->
-
-                    <!-- Listing Item -->
-                    <div class="nj-listings__item">
-                        <div class="nj-listings__card">
-                            <img class="nj-thumb" src="assets/img/spa/l-1.jpeg" alt="">
-                            <div class="nj-content">
-                                <span class="tag tag--lime">Save as much as 15%</span>
-                                <div class="nj-action-widget">
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-heart"></i></button>
-                                    <button class="btn btn--circle btn--lightblue"><i class="fa-solid fa-share-nodes"></i></button>
-                                </div>
-                                <img src="assets/img/badge/top-rated.png" class="nj-badge" />
-                                <span class="nj-location"><i class="fa-solid fa-location-dot me-1"></i>Station 2, Boracay</span>
-                            </div>
-                        </div>
-                        <div class="p-3">
-                            <h3 class="nj-title">Bella Isa Salon & Spa</h3>
-                            <div class="nj-price-wrapper">
-                                <span class="nj-label">Starts</span>
-                                <span class="nj-price">P1,000</span>
-                            </div>
-                            <div class="nj-excerpt line-clamp">
-                                <p class="">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates Quae maxime repellendus unde sit, libero laborum? Illo molestias odit commodi voluptates
-                                </p>
-                            </div>
-
-                            <div class="nj-engagement d-flex justify-content-between">
-                                <div>
-                                    <span class="nj-engagement__reacts me-2"><i class="fa-regular fa-face-smile me-1"></i>3262</span>
-                                    <span class="nj-engagement__reacts"><i class="fa-regular fa-face-frown me-1"></i>76</span>
-                                </div>
-                                <span class="nj-engagement__replies"><i class="fa-solid fa-reply me-1"></i>234 Replies</span>
-                            </div>
-                            <div class="text-center mt-2">
-                                <a href="#" class="btn btn--blue">Book Now</a>
-
-                            </div>
-                        </div>
-
-                    </div>
-                    <!--/Listing Item -->
                 </div>
             </div>
         </div>
